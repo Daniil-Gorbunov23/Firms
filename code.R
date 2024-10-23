d0113a31c9d8a09f7ed880febcef39f93aeb782f
avlsv <avlasov@nes.ru>
2024-10-23 15:30:52 +0300

    Started R file
    Started writing R file and added datasets for deciphering the OKTAO codes

---
+++ code.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(data.table)
+library(tidytable)
+dataset_tbl <- 
+  read_csv("OriginalDataset/Data.csv")
+
+dataset_dttbl <-
+  dataset_tbl|> as.data.table()
+
+
+dataset_dttbl|> names()
+
+#keep y k l18 sector year okato_regioncode services manuf
+
+dataset_tbl |> 
+  select(y_gr18, m18, k18, l18, year, okato_regioncode, services, manuf)
